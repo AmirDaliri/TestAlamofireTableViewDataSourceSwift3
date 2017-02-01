95c748bb791bd5eb75e09178d2cca68842501f4f
Amir Daliri <amir.dalirii69@yahoo.com>
2017-02-01 18:27:32 +0330

    initial

---
+++ Video.swift
@@ -0,0 +1,20 @@
+//
+//  Video.swift
+//  testAPI
+//
+//  Created by Amir Daliri on 1/31/17.
+//  Copyright © 2017 Amir Daliri. All rights reserved.
+//
+
+import Foundation
+
+
+class Video {
+    var title : String
+    var username : String
+    
+    init(title : String , username: String) {
+        self.title = title
+        self.username = username
+    }
+}
--- testAPI/ViewController.swift
+++ testAPI/ViewController.swift
@@ -7,19 +7,61 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , UITableViewDataSource {
+    
+    var videos = [Video]()
+    
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        Alamofire.request("http://www.aparat.com/etc/api/mostviewedvideos").validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                
+                let json = JSON(value)
+                
+                for (_,subJson) : (String , JSON) in json["mostviewedvideos"] {
+                    let temp = Video(title: subJson["title"].string!, username: subJson["username"].string!)
+                    self.videos.append(temp)
+                }
+                
+                
+                self.tableView.reloadData()
+                
+            case .failure(let error):
+                print(error)
+                
+            }
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return videos.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = videos[indexPath.row].title
+        cell.detailTextLabel?.text = videos[indexPath.row].username
+        
+        
+        
+        return cell
+    }
+    
+    
+    
 }
 
